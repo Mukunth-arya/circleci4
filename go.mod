f93f7666f9676541d77947f877c0d45463d8de3f
Mukunth-arya <mukunthnataraj@gmail.com>
2022-07-14 15:42:27 +0530

    sampleci

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Mukunth-arya/circleci4.git
+
+go 1.18
---
+++ sample_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+)
+
+func Testsample(t testing.T) error {
+
+	var data = []int{}
+
+	if data == nil {
+		t.Error("sorry")
+	}
+	return nil
+
+}
